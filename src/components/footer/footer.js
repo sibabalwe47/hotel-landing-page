8471b9b5dfdf6a4889a0ab103b00337a1f3db8ee
Siba <sibabalwe47@gmail.com>
2021-08-18 08:31:18 +0200

    Set up footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,19 @@
+import react from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <div className="col-md-3">
+                    logo    
+                </div>
+                <div className="col-md-6">
+                    Links
+                </div>
+                <div className="col-md-3">
+                    Social links
+                </div>
+            </div>
+        </footer>
+    )
+}
