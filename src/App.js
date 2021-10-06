a67914f9c92569f629ebc7ff2befc8a6eb7d69db
Siba <sibabalwe47@gmail.com>
2021-10-06 02:18:27 +0200

    sass to css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.scss';
-import LandingPage from './views/LandingPage'
-import Navigation from './components/navigation/navigation'
-import { Fragment } from 'react';
-import {Provider} from 'react-redux'
-import store from './store/store'
-import Test from './components/test/test';
+import "./style.css";
+import "./App.css";
+import LandingPage from "./views/LandingPage";
+import Navigation from "./components/navigation/navigation";
+import { Fragment } from "react";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import Test from "./components/test/test";
 
 function App() {
   return (
-   <Provider store={store}>
+    <Provider store={store}>
       <Fragment>
         <Navigation />
         <LandingPage />
       </Fragment>
-   </Provider>
-  )
+    </Provider>
+  );
 }
 
 export default App;
